4c15e82a5eacf3b1cd69918706b2da58d1a7b328
Mat Kelcey <matthew.kelcey@gmail.com>
2019-05-26 22:30:13 +1000

    add tensorboard logging

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import callbacks
 import argparse
 import data
 import model as m
@@ -15,9 +15,12 @@ parser.add_argument('--learning-rate', type=float, default=1e-3, help="learning 
 parser.add_argument('--margin', type=float, default=1e-3, help="hinge loss margin")
 parser.add_argument('--epochs', type=int, default=20)
 parser.add_argument('--steps-per-epoch', type=int, default=20)
-parser.add_argument('--model-output', type=str, default='model', help='where to save model')
+parser.add_argument('--output-dir', type=str, default='.',
+                    help='dir to save model.hdf5, tb logs, etc')
 opts = parser.parse_args()
 print(opts)
+
+u.ensure_dir_exists(opts.output_dir)
 
 examples = data.a_p_n_iterator(batch_size=opts.batch_size,
                                img_dir=opts.img_dir)
@@ -29,14 +32,13 @@ m.compile(model,
           learning_rate=opts.learning_rate,
           margin=opts.margin)
 
-u.ensure_dir_exists(os.path.dirname(opts.model_output))
-save_checkpoint = ModelCheckpoint(filepath=opts.model_output)
-
+callbacks = [callbacks.ModelCheckpoint(filepath="%s/model.hdf5" % opts.output_dir),
+             callbacks.TensorBoard(log_dir="%s/logs" % opts.output_dir)]
 model.fit(examples,
           epochs=opts.epochs,
           verbose=1,
           steps_per_epoch=opts.steps_per_epoch,
-          callbacks=[save_checkpoint])
+          callbacks=callbacks)
 
 
 
