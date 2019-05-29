7a0d96bb266788e6a38274e1dbdc220dad936103
Mat Kelcey <matthew.kelcey@gmail.com>
2019-05-29 15:28:10 +1000

    generate toy data across 2 runs for consistent testing or N camera, R runs, F frames

--- generate_toy_rgb_data.py
+++ generate_toy_rgb_data.py
@@ -10,23 +10,22 @@ import util as u
 
 # generate synthetic toy data where each frame is it's own colour.
 for frame_num, colour in enumerate(['#ff0000', '#ffff00', '#00ff00',
-                                    '#00ffff', '#0000ff', '#ff00ff']):    
-    for camera_id in range(10):
-        output_dir = "imgs/c%02d/r01/" % camera_id
-        u.ensure_dir_exists(output_dir)
+                                    '#00ffff', '#0000ff', '#ff00ff']):
+    for run_id in range(2):
+        for camera_id in range(5):
+            output_dir = "imgs/00_rgb/c%02d/r%02d/" % (camera_id, run_id)
+            u.ensure_dir_exists(output_dir)
 
-        img = Image.new('RGB', (W, H), (0, 0, 0))
-        canvas = ImageDraw.Draw(img)
-        
-        # choose rectangle of min size
-        rectangle_area = 0
-        while rectangle_area < 1000:
-            x0, x1 = np.random.randint(0, W, size=2)
-            y0, y1 = np.random.randint(0, H, size=2)
-            rectangle_area = np.abs((x1-x0) * (y1-y0))
-            print("frame_num", frame_num, "rectangle_area", rectangle_area)
-        canvas.rectangle([x0,y0,x1,y1], fill=colour)
+            img = Image.new('RGB', (W, H), (0, 0, 0))
+            canvas = ImageDraw.Draw(img)
 
-        img.save("%s/f%03d.png" % (output_dir, frame_num))
-       
-    
+            # choose rectangle of min size
+            rectangle_area = 0
+            while rectangle_area < 1000:
+                x0, x1 = np.random.randint(0, W, size=2)
+                y0, y1 = np.random.randint(0, H, size=2)
+                rectangle_area = np.abs((x1-x0) * (y1-y0))
+                print("frame_num", frame_num, "rectangle_area", rectangle_area)
+            canvas.rectangle([x0,y0,x1,y1], fill=colour)
+
+            img.save("%s/f%03d.png" % (output_dir, frame_num))
