a3661760865289a1144aca53ca6b8e14ca38309e
Mat Kelcey <matthew.kelcey@gmail.com>
2019-05-29 16:29:40 +1000

    speed up embedding by feeding with a tf.data pipeline

--- embed_imgs.py
+++ embed_imgs.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import util as u
 import sys
+import tensorflow as tf
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--manifest', type=str, default='manifest', help='list of files to embed')
@@ -20,34 +21,35 @@ opts = parser.parse_args()
 _, model = m.construct_model(embedding_dim=opts.embedding_dim)
 model.load_weights(opts.model_input)
 
-# next batch of images to run through
-img_batch = []
-
-# TODO: do this as a tf.data pipeline for parallelisation
-
 filenames = list(u.slurp_manifest(opts.manifest))
+
+def filenames_generator():
+    for filename in filenames:
+        yield filename
+
+def decode_img(img_name):
+    return tf.image.decode_jpeg(tf.read_file(img_name))   # (H, W, 3)   uint8
+
+iter = (tf.data.Dataset.from_generator(filenames_generator, output_types=(tf.string))
+        .map(decode_img, num_parallel_calls=8)
+        .batch(64)
+        .prefetch(tf.contrib.data.AUTOTUNE)
+        .make_one_shot_iterator()
+        .get_next())
 
 # prealloc embeddings and copy them in per batch
 embeddings = np.empty((len(filenames), opts.embedding_dim))
 e_offset = 0
 
-for i, filename in enumerate(filenames):
-    # decode image
-    pil_img = Image.open(filename)
-    img_batch.append(np.array(pil_img))
-    # if enoguh images run through batch
-    if len(img_batch) == opts.batch_size:
-        predictions = model.predict(np.stack(img_batch))
-        embeddings[e_offset:e_offset+len(img_batch)] = predictions
-        e_offset += len(img_batch)
-        img_batch = []
-        sys.stdout.write("%d/%d                 \r" % (i, len(filenames)))
+sess = tf.Session()
+while True:
+    try:
+        imgs = sess.run(iter)
+        next_batch = model.predict(imgs)
+        embeddings[e_offset:e_offset+len(next_batch)] = next_batch
+        e_offset += len(next_batch)
+        sys.stdout.write("%d/%d                 \r" % (e_offset, len(filenames)))
+    except tf.errors.OutOfRangeError:
+        break
 
-# flush final batch
-if len(img_batch) > 0:
-    predictions = model.predict(np.stack(img_batch))
-    embeddings[e_offset:e_offset+len(img_batch)] = predictions
-
-# flush embeddings
-print("embeddings.shape", embeddings.shape)
 np.save(opts.embeddings_output, embeddings)
