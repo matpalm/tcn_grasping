f8c849ee96d153e58fce988432668ca791c1396c
Mat Kelcey <matthew.kelcey@gmail.com>
2019-06-01 12:00:29 +1000

    switch debug config to img 03 dataset

--- debug_calc_embedding_near_neighbours.sh
+++ debug_calc_embedding_near_neighbours.sh
@@ -2,17 +2,17 @@
 set -ex
 
 # take snapshop of model (in case still training)
-cp runs/$1/model.hdf5 /tmp/
+cp runs/$1/model.hdf5 /tmp
 
 # embed images from reference sequence
 time ./embed_imgs.py \
---manifest imgs/02_20c_10o.lores/c00.r00.manifest \
+--manifest imgs/03/c087.manifest \
 --model-input /tmp/model.hdf5 \
 --embedding-dim 64 \
---embeddings-output runs/$1/02_20c_10o.lores.c00.r00.embeddings.npy  # (800, 32)
+     --embeddings-output runs/$1/03.c087.embeddings.npy
 
 # embed all images from entire test set
-##time ./embed_imgs.py \
+#time ./embed_imgs.py \
 ##--manifest imgs/02_20c_10o.lores/test.manifest \
 ##--model-input runs/$1/model.hdf5 \
 ##--embedding-dim 32 \
@@ -20,36 +20,36 @@ time ./embed_imgs.py \
 
 # embed images from test set, c18 and c19, each
 time ./embed_imgs.py \
---manifest imgs/02_20c_10o.lores/c18.manifest \
+--manifest imgs/03/c088.manifest \
 --model-input /tmp/model.hdf5 \
 --embedding-dim 64 \
---embeddings-output runs/$1/02_20c_10o.lores.c18.embeddings.npy
+--embeddings-output runs/$1/03.c088.embeddings.npy
 time ./embed_imgs.py \
---manifest imgs/02_20c_10o.lores/c19.manifest \
+--manifest imgs/03/c089.manifest \
 --model-input /tmp/model.hdf5 \
 --embedding-dim 64 \
---embeddings-output runs/$1/02_20c_10o.lores.c19.embeddings.npy
+--embeddings-output runs/$1/03.c089.embeddings.npy
 
 # do near neighbour calcs between (ref, c18) & (ref, c19)
 ./debug_embedding_near_neighbours.py \
---manifest-a imgs/02_20c_10o.lores/c00.r00.manifest \
---embeddings-a runs/$1/02_20c_10o.lores.c00.r00.embeddings.npy \
---manifest-b imgs/02_20c_10o.lores/c18.manifest \
---embeddings-b runs/$1/02_20c_10o.lores.c18.embeddings.npy > /tmp/c18.nns &
+--manifest-a imgs/03/c087.manifest \
+--embeddings-a runs/$1/03.c087.embeddings.npy \
+--manifest-b imgs/03/c088.manifest \
+--embeddings-b runs/$1/03.c088.embeddings.npy > /tmp/c88.nns &
 ./debug_embedding_near_neighbours.py \
---manifest-a imgs/02_20c_10o.lores/c00.r00.manifest \
---embeddings-a runs/$1/02_20c_10o.lores.c00.r00.embeddings.npy \
---manifest-b imgs/02_20c_10o.lores/c19.manifest \
---embeddings-b runs/$1/02_20c_10o.lores.c19.embeddings.npy > /tmp/c19.nns &
+--manifest-a imgs/03/c087.manifest \
+--embeddings-a runs/$1/03.c087.embeddings.npy \
+--manifest-b imgs/03/c089.manifest \
+--embeddings-b runs/$1/03.c089.embeddings.npy > /tmp/c89.nns &
 wait
 
 # join into one file, sample and stitch
-join /tmp/c18.nns /tmp/c19.nns | awk 'NR%10==0' | ./stitch.py
+join /tmp/c88.nns /tmp/c89.nns | awk 'NR%10==0' | ./stitch.py
 
 # make a gif
 convert stitch*png runs/$1/near_neighbour_egs.gif
 
 # clean up
-rm stitch*png /tmp/{c18,c19}.nns /tmp/model.hdf5
+rm stitch*png /tmp/{c88,c89}.nns /tmp/model.hdf5
 
 eog runs/$1/near_neighbour_egs.gif
